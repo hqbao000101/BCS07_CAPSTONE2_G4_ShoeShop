d5125bb5a541af94919f24f6e3e0d8a23cd7eb24
hqbao000101 <hbao592000@gmail.com>
2023-06-09 19:09:17 +0700

    feat: header

---
+++ controllers/main.js
@@ -0,0 +1,10 @@
+// todo: menu
+// document.getElementById("showMenu").onclick = () => {
+//   document.querySelector("#home_header .bg-overlay").classList.toggle("d-none");
+//   document.querySelector("#home_header .sidenav").classList.toggle("d-none");
+// };
+
+// document.getElementById("close-icon").onclick = () => {
+//   document.querySelector("#home_header .bg-overlay").classList.toggle("d-none");
+//   document.querySelector("#home_header .sidenav").classList.toggle("d-none");
+// };
