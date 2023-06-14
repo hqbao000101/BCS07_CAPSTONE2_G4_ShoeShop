1d9a888df7108414608a5207dfc04a22916706ce
hqbao000101 <hbao592000@gmail.com>
2023-06-14 18:33:14 +0700

    feat: home-load-shoes-api

---
+++ controllers/helper.js
@@ -0,0 +1,67 @@
+// todo: render-home-shoes
+function renderShoes(arr) {
+  let categories_list = ["all", "men", "women", "adidas", "nike", "converse"];
+  for (let i = 0; i < categories_list.length; i++) {
+    document.querySelector(`#${categories_list[i]} .row`).innerHTML = arr[i];
+  }
+}
+
+// todo: filter shoes by categories
+function filterCategory(shoeArr, category) {
+  let result = [];
+  for (let item of shoeArr) {
+    let categories = JSON.parse(item.categories);
+    for (let i = 0; i < categories.length; i++) {
+      if (categories[i].category.toLowerCase() == category) {
+        result.push(item);
+      }
+    }
+  }
+  return result;
+}
+
+// todo: update content for different shoes' categories
+function loopUpdateContent(arr, stt, result) {
+  for (let item of arr) {
+    result += `
+    <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+      <div class="product_item animate__animated animate__zoomIn">
+        <div class="sales_container">
+          <!-- todo: sale-img -->
+          <div class="sales_img">
+            <img
+              src=${item.image}
+              alt="Sales Pic ${stt}"
+            />
+            <div class="img_interact">
+              <i class="fa-regular fa-heart"></i>
+              <i class="fa-regular fa-images"></i>
+            </div>
+            <div class="img_add">
+              <h5>QUICK ADD</h5>
+            </div>
+          </div>
+          <!-- todo: sale-info -->
+          <div class="sales_info">
+            <a href="../views/detail.html">
+              <h4>${item.name}</h4>
+            </a>
+            <div class="info_stars">
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>
+            </div>
+            <div class="info_prices">
+              <h5>$${item.price}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+    stt++;
+  }
+  return result;
+}
--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,47 @@
-// todo: menu
-// document.getElementById("showMenu").onclick = () => {
-//   document.querySelector("#home_header .bg-overlay").classList.toggle("d-none");
-//   document.querySelector("#home_header .sidenav").classList.toggle("d-none");
-// };
+// todo: get shoe list
+let promise = axios({
+  method: "GET",
+  url: "https://shop.cyberlearn.vn/api/Product",
+});
+promise.then((res) => {
+  // todo: get list of shoes
+  let shoe_arr = res.data.content;
 
-// document.getElementById("close-icon").onclick = () => {
-//   document.querySelector("#home_header .bg-overlay").classList.toggle("d-none");
-//   document.querySelector("#home_header .sidenav").classList.toggle("d-none");
-// };
+  // todo: initialize variables containing shoe items
+  let stt = 1;
+  let all = '';
+  let men = '';
+  let women = '';
+  let adidas = '';
+  let nike = '';
+  let converse = '';
+
+  // todo: update content for all shoe section
+  all = loopUpdateContent(shoe_arr, stt, all);
+
+  // todo: update content for men shoe section
+  let men_shoe_arr = filterCategory(shoe_arr, "men");
+  men = loopUpdateContent(men_shoe_arr, stt, men);
+
+  // todo: update content for women shoe section
+  let women_shoe_arr = filterCategory(shoe_arr, "women");
+  women = loopUpdateContent(women_shoe_arr, stt, women);
+
+  // todo: update content for adidas shoe section
+  let adidas_shoe_arr = filterCategory(shoe_arr, "adidas");
+  adidas = loopUpdateContent(adidas_shoe_arr, stt, adidas);
+
+  // todo: update content for nike shoe section
+  let nike_shoe_arr = filterCategory(shoe_arr, "nike");
+  nike = loopUpdateContent(nike_shoe_arr, stt, nike);
+
+  // todo: update content for converse shoe section
+  let converse_shoe_arr = filterCategory(shoe_arr, "vans converse");
+  converse = loopUpdateContent(converse_shoe_arr, stt, converse);
+  
+  // todo: rendering shoes following shoes categories
+  renderShoes([all, men, women, adidas, nike, converse]);
+});
+promise.catch((err) => {
+  console.log(err);
+});
